b1eab4f63206e98f3f02d180c049f99118049bef
Yana-Denisova <ladymacbeth@list.ru>
2022-11-16 22:22:10 +0300

    added index_view + redirect_view + flash massages

--- yacut/views.py
+++ yacut/views.py
@@ -1,23 +1,37 @@
-from flask import render_template, flash
+import random
+import string
+
+from flask import render_template, flash, redirect, url_for
 
 from . import app, db
 from .forms import UrlCutForm
 from .models import URL_map
 
 
-#def get_unique_short_id():
+def get_unique_short_id():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
 
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = UrlCutForm()
     if form.validate_on_submit():
-        original_link = form.original_link.data
-        custom_id = form.custom_id.data
-        if URL_map.query.filter_by(custom_id=custom_id).first():
-            flash('Такое вариант короткой ссылки уже существует!')
-            return render_template('index.html', form=form)
-        new_url = URL_map(
-            original = original_link,
-            short = custom_id
-        )
+        original_url = form.original_link.data
+        custom_url = form.custom_id.data
+        if not custom_url or custom_url == '' or custom_url is None:
+            custom_url = get_unique_short_id()
+        if URL_map.query.filter_by(short=form.custom_id.data).first():
+            flash('Такой вариант короткой ссылки уже существует!', 'fail')
+        else:
+            new_url = URL_map(
+            original = original_url,
+            short = custom_url
+            )
+            db.session.add(new_url)
+            db.session.commit()
+            flash(url_for('redirect_view', id = custom_url, _external=True), 'short_url')
     return render_template('index.html', form=form)
+
+@app.route('/<string:id>')
+def redirect_view(id):
+    link = URL_map.query.filter_by(short=id).first()
+    return redirect(link.original)
