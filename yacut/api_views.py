079878a104d4b3c8673e21d8ae4e4460ed037dbd
Yana-Denisova <ladymacbeth@list.ru>
2022-11-17 22:31:28 +0300

    added get_url api view

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,5 +1,4 @@
 from flask import jsonify, request
-
 from . import app, db
 from .models import URL_map
 
@@ -12,3 +11,8 @@ def add_url():
     db.session.add(url)
     db.session.commit()
     return jsonify(url.to_dict()), 201
+
+@app.route('/api/id/<string:short_id>/', methods=['GET'])
+def get_url(short_id):
+    link = URL_map.query.filter_by(short=short_id).first()
+    return jsonify({'url': link.original}), 200
